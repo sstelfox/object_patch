0caeb7d3c850435b23fa72acb67f783a4e9f0c89
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2013-09-30 10:18:52 -0400

    Fleshed out the test operation a tad

--- lib/object_patch/operations/test.rb
+++ lib/object_patch/operations/test.rb
@@ -3,11 +3,25 @@ module ObjectPatch
   module Operations
     class Test
       def initialize(patch_hash)
-        @patch = patch_hash
+        @path = ObjectPatch::Pointer.decode(patch_hash.fetch("path"))
+        @value = patch_hash.fetch("value")
       end
 
       def apply(source_hash)
-        source_hash
+        recursive_test(source_hash, @path, @value)
+      end
+
+      def recursive_test(obj, path, test_value)
+        if path.nil? || path.empty?
+          raise ArgumentError unless (obj == test_value)
+          return
+        end
+
+        # Grab our key off the stack
+        key = path.shift
+        raise ArgumentError if key.nil?
+
+        recursive_test(obj[key], path, test_value)
       end
     end
   end
