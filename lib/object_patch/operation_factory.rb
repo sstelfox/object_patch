6b675e01e6dec0124b6b1ffa009a46d084268299
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2013-10-01 14:07:01 -0400

    Down too 66 errors

--- lib/object_patch/operation_factory.rb
+++ lib/object_patch/operation_factory.rb
@@ -3,7 +3,7 @@ module ObjectPatch
   module OperationFactory
     def process(operation)
       const_spot = operation.delete("op").capitalize.to_sym
-      raise InvalidOperation unless ObjectPatch::Operation.const_defined?(const_spot)
+      raise InvalidOperation unless ObjectPatch::Operations.const_defined?(const_spot)
 
       ObjectPatch::Operations.const_get(const_spot).new(operation)
     end
--- lib/object_patch/operations/remove.rb
+++ lib/object_patch/operations/remove.rb
@@ -3,11 +3,29 @@ module ObjectPatch
   module Operations
     class Remove
       def initialize(patch_hash)
-        @patch = patch_hash
+        @path = ObjectPatch::Pointer.decode(patch_hash.fetch("path"))
       end
 
       def apply(source_hash)
-        source_hash
+        recursive_remove(source_hash, @path)
+      end
+
+      def recursive_remove(obj, path)
+        raise ArgumentError unless key = path.shift
+        key_type = obj.class
+
+        key = key.to_i if key_type == Array
+        raise ArgumentError if key_type == Array && obj.size >= key
+        raise ArgumentError if key_type == Hash && !obj.keys.include?(key)
+
+        if path.empty?
+          obj.delete_at(key) if key_type == Array
+          obj.delete!(path.first) if key_type == Hash
+        else
+          obj[key] = recursive_remove(obj[key], path)
+        end
+
+        obj
       end
     end
   end
