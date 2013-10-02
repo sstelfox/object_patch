751a6ebb665bfdfcbd35ea7a8e1a83dfb4a8fbf4
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2013-10-02 11:47:06 -0400

    Added new exception

--- lib/object_patch/exceptions.rb
+++ lib/object_patch/exceptions.rb
@@ -2,10 +2,12 @@
 module ObjectPatch
   BaseException = Class.new(StandardError)
 
-  IndexError          = Class.new(BaseException)
-  InvalidOperation    = Class.new(BaseException)
-  MissingKeyError     = Class.new(BaseException)
-  TestOperationFailed = Class.new(BaseException) do
+  IndexError                  = Class.new(BaseException)
+  InvalidOperation            = Class.new(BaseException)
+  MissingKeyError             = Class.new(BaseException)
+  ObjectOperationOnArrayError = Class.new(BaseException)
+
+  class TestOperationFailed < BaseException
     def initialize(path, value)
       super("Expected #{value} at #{path}")
     end
