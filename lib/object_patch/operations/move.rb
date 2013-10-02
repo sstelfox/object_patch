3ca8931f02082c4616d71fe063cee9dd7b210129
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2013-10-02 14:09:36 -0400

    Yeah fixed that...

--- lib/object_patch/operations/move.rb
+++ lib/object_patch/operations/move.rb
@@ -3,12 +3,12 @@ module ObjectPatch
   module Operations
     class Move
       def initialize(patch_hash)
-        @from = ObjectPatch::Pointer.parse(patch_hash.fetch("from"))
-        @to = ObjectPatch::Pointer.parse(patch_hash.fetch("path"))
+        @from = patch_hash.fetch("from")
+        @to = patch_hash.fetch("path")
       end
 
       def apply(source_hash)
-        current = ObjectPatch::Pointer.eval(@from, source_hash)
+        current = ObjectPatch::Pointer.eval(ObjectPatch::Pointer.parse(@from), source_hash)
         source_hash = Remove.new({'path' => @from}).apply(source_hash)
         Add.new({'path' => @to, 'value' => current}).apply(source_hash)
       end
