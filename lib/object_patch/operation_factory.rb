55916bb7c1f91be00e9d5c44b3191e87547dfd59
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2013-10-03 00:15:42 -0400

    More documentation

--- lib/object_patch/operation_factory.rb
+++ lib/object_patch/operation_factory.rb
@@ -1,6 +1,14 @@
 
 module ObjectPatch
+
+  # A factory module used to build the appropriate operation objects.
   module OperationFactory
+
+    # Build an operation object that matches the operation type and makes the
+    # appropriate information available to them.
+    #
+    # @param [Hash] patch
+    # @return [Object] One of the operations classes.
     def build(patch)
       op_const = patch['op'].capitalize.to_sym
 
