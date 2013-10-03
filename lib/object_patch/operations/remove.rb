0d86b0337110618f2067d0a5bc44b17ea37d61ce
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2013-10-02 22:23:16 -0400

    Refactored remove operation

--- lib/object_patch/operations/remove.rb
+++ lib/object_patch/operations/remove.rb
@@ -2,17 +2,24 @@
 module ObjectPatch::Operations
   class Remove
     def initialize(patch_data)
-      @patch_data = patch_data
+      @path = patch_data.fetch('path')
     end
 
     def apply(target_doc)
-      list = ObjectPatch::Pointer.parse(@patch_data['path'])
-      key  = list.pop
-      obj  = ObjectPatch::Pointer.eval(list, target_doc)
+      key = processed_path.last
+      inner_obj = ObjectPatch::Pointer.eval(processed_path[0...-1], target_doc)
 
-      ObjectPatch::Operations.rm_op(obj, key)
+      ObjectPatch::Operations.rm_op(inner_obj, key)
 
       target_doc
+    end
+
+    def processed_path
+      ObjectPatch::Pointer.parse(@path)
+    end
+
+    def to_patch
+      { 'op' => 'remove', 'path' => @path }
     end
   end
 end
