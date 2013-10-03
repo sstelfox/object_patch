f4578c6e24fb3870b972b1e031d3a3fb28d42cfb
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2013-10-03 00:44:42 -0400

    Even more documentation

--- lib/object_patch/operations/replace.rb
+++ lib/object_patch/operations/replace.rb
@@ -1,11 +1,16 @@
 
 module ObjectPatch::Operations
-  class Replace
-    def initialize(patch_data)
-      @path  = patch_data.fetch('path')
-      @value = patch_data.fetch('value')
-    end
 
+  # A representation of a JSON pointer replace operation.
+  class Replace
+
+    # Apply this operation to the provided document and return the updated
+    # document. Please note that the changes will be reflected not only in the
+    # returned value but the original document that was passed in as well.
+    #
+    # @param [Object] target_doc The document that will be modified by this
+    #   patch.
+    # @return [Object] The modified document
     def apply(target_doc)
       return @value if processed_path.empty?
 
@@ -22,10 +27,30 @@ module ObjectPatch::Operations
       target_doc
     end
 
+    # Setup the replace operation with any required arguments.
+    #
+    # @param [Hash] patch_data Parameters necessary to build the operation.
+    # @option patch_data [String] path The location in the target document to
+    #   remove.
+    # @option patch_data [String] value The value that should be written over
+    #   whatever is at the provided path.
+    # @return [void]
+    def initialize(patch_data)
+      @path  = patch_data.fetch('path')
+      @value = patch_data.fetch('value')
+    end
+
+    # Returns the path after being expanded by the JSON pointer semantics.
+    #
+    # @return [Array<String>] Expanded pointer path
     def processed_path
       ObjectPatch::Pointer.parse(@path)
     end
 
+    # Covert this operation to a format that can be built into a full on JSON
+    # patch.
+    #
+    # @return [Hash<String => String>] JSON patch replace operation
     def to_patch
       { 'op' => 'replace', 'path' => @path, 'value' => @value }
     end
